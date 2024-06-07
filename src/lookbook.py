2acc9296fdc602b18d62c2e7f3480157e7ba6f62
Boseong <lee@paperst.ar>
2024-06-07 20:20:44 +0900

    Update lookbook.py

--- src/lookbook.py
+++ src/lookbook.py
@@ -128,17 +128,17 @@ def get_lookbook(gender: str= "", ageRange: str= "", area: dict[str, str]= {"pro
 ## 출력 결과
 - 1024x1792픽셀
 - 깔끔한 배경에서 찍은 전신 사진
-- 머리부터 발끝까지 나온 전신
+- 머리부터 발끝까지 나온 전신 하나
 - 사람 외에 다른 사물은 없어야 함
 - 한국인 모델 한명
 - 전신 사진 외 다른 구도는 없어야 함
 
-## context
-- 자외선 농도: {get_uv(areaNo)}
-- 날씨: {sensed_temperature}
-- 상황: {TPO_string}
-- 나이: {ageRange}
-- 성별: {gender}
+## 입력
+- 자외선 농도: '{get_uv(areaNo)}'
+- 날씨: '{sensed_temperature}'
+- 상황: '{TPO_string}'
+- 나이: '{ageRange}'
+- 성별: '{gender}'
     """.strip()
     
     try:
